0e55dc0ec42b08edec47f0def9e2037934e77a71
fafa <fafa@fafas-MacBook-Air.local>
2020-11-24 16:24:07 +0900

    tyotto dake

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@
 #include <Eigen/Dense>
 #include <Eigen/LU>
 #include <chrono>
+#include <assert.h>
 
 
 void test(const int &a){
@@ -61,33 +62,52 @@ int main(){
     i1 = edge(1, i);
 
     if((i0 >= 0 && i0 < n) && (i1 >= 0 && i1 < n)){
-//      xn(1, i) = x(2, i1) - x(2, i0);
-//      xn(2, i) = x(1, i0) - x(1, i1);
+      xn(0, i) = x(1, i1) - x(1, i0);
+      xn(1, i) = x(0, i0) - x(0, i1);
 
-//      hs(i) = std::sqrt(std::pow(xn(1, i), 2.0) + std::pow(xn(2, i), 2.0));
+      hs(i) = std::sqrt(std::pow(xn(0, i), 2.0) + std::pow(xn(1, i), 2.0));
+
+      xn(0, i) = xn(0, i)/hs(i);
+      xn(1, i) = xn(1, i)/hs(i);
+//      std::cout << "i, xn(0, i), xn(1, i) " << i << ' ' << xn(0, i) << ' '<< xn(1, i) << std::endl; //dbg
+//      std::cout << "hs(i) " << hs(i) << std::endl; //dbg
     }
+    else{
+      assert(false);
+    }
+    // x**3*y - x*y**3
+    u(i) = std::pow(x(0, i), 3.0)*x(1, i) - x(0, i)*std::pow(x(1, i), 3.0);
+
+    // (3x**2*y - y**3)nx + (x**3 - 3xy**2)ny
+    kai(i) = (3.0*std::pow(x(0, i), 2.0)*x(1, i) - std::pow(x(1, i), 3.0))*xn(0, i)
+      + (std::pow(x(0, i), 3.0) - 3.0*x(0, i)*std::pow(x(1, i), 2.0))*xn(1, i);
+
+    std::cout << "i, u(i), kai(i) " << i << ' ' << u(i) << ' '<< kai(i) << std::endl; //dbg
+  }
+
+  for(j = 0; j < n; j++){
+     j0 = edge(0, j);
+     j1 = edge(1, j);
+     for(i = 0; i < n; i++){
+       i0 = edge(0, i);
+       i1 = edge(1, i);
+//        xm(:) = 0.5d0*(x(:,i0) + x(:,i1))
+//        lp1(i, j) = dble(slp_laplace(xm, x(:,j0), x(:,j1), hs(j), xn(:,j)))
+//        lp2(i, j) = dble(dlp_laplace(xm, x(:,j0), x(:,j1), hs(j), xn(:,j), exterior))
+     }
   }
 }
 
-
-//  do i = 1, n
-//     i0 = edge(1, i)
-//     i1 = edge(2, i)
-//     if((i0 .ge. 1 .and. i0 .lt. n+1) .and. (i1 .ge. 1 .and. i1 .lt. n+1)) then
-//        xn(1, i) = x(2, i1) - x(2, i0)
-//        xn(2, i) = x(1, i0) - x(1, i1)
+//  exterior = 0
 // 
-//        hs(i) = sqrt(xn(1, i)**2 + xn(2, i)**2)
-// 
-//        xn(1, i) = xn(1, i)/hs(i)
-//        xn(2, i) = xn(2, i)/hs(i)
-//     else
-//        call force_raise()
-//     end if
-// 
-//     !x**3*y - x*y**3
-//     u(i) = x(1, i)**3*x(2, i) - x(1, i)*x(2, i)**3
-// 
-//     !(3x**2*y - y**3)nx + (x**3 - 3xy**2)ny
-//     kai(i) = (3*x(1, i)**2*x(2, i) - x(2, i)**3)*xn(1, i) + (x(1, i)**3 - 3*x(1, i)*x(2, i)**2)*xn(2, i)
+//  do j = 1, n
+//     j0 = edge(1, j)
+//     j1 = edge(2, j)
+//     do i = 1, n
+//        i0 = edge(1, i)
+//        i1 = edge(2, i)
+//        xm(:) = 0.5d0*(x(:,i0) + x(:,i1))
+//        lp1(i, j) = dble(slp_laplace(xm, x(:,j0), x(:,j1), hs(j), xn(:,j)))
+//        lp2(i, j) = dble(dlp_laplace(xm, x(:,j0), x(:,j1), hs(j), xn(:,j), exterior))
+//     end do
 //  end do
